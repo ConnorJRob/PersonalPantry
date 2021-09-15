58660655db119a18317e62267c6bb4361b4708cb
Connor <connorjrobertson@hotmail.co.uk>
2021-09-15 14:27:48 +0100

    Initial commit

---
+++ src/test/java/com/personalpantry/example/PersonalPantry/PersonalPantryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalpantry.example.PersonalPantry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalPantryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
